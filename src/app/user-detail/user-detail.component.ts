61c01051e3093f14dc02f5fdb617909860564602
SebSeitz <sebastian_seitz13@web.de>
2022-02-08 17:15:16 +0100

    edit address component

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/models/user.class';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+import { DialogEditAddressComponent } from '../dialog-edit-address/dialog-edit-address.component';
+import { DialogEditUserComponent } from '../dialog-edit-user/dialog-edit-user.component';
 
 @Component({
   selector: 'app-user-detail',
@@ -7,15 +13,40 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./user-detail.component.scss']
 })
 export class UserDetailComponent implements OnInit {
-  userId ='';
+  userId = '';
+  user: User = new User();
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private firestore: AngularFirestore, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(paramMap => {
       this.userId = paramMap.get('id');
       console.log('Got ID', this.userId);
-
+      this.getUser();
     })
+  }
+
+  getUser(){
+    this.firestore
+    .collection('users')
+    .doc(this.userId)
+    .valueChanges()
+    .subscribe((user: any) =>{
+      this.user = new User(user); // das JSON user wird in ein Objekt vom Typ User umgewandelt
+      console.log('Retrieved User', this.user);
+    })
+  }
+
+  openAddressDialog(){
+
+  }
+
+  editUserDetail(){
+    this.dialog.open(DialogEditUserComponent);
+  }
+
+  editMenu(){
+    const dialog = this.dialog.open(DialogEditAddressComponent);
+    dialog.componentInstance.user = this.user;
   }
 }
